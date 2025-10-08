777589f79926151437a332cb10d3ca76f2e2371b
Alina-lgth <tarasova-0211av@mail.ru>
2025-10-08 18:20:49 +0300

    nev commit

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<hl> Заголовок страницы</hl>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Моя страница </title>
+		<link rel="stylesheet" href='mycss.css'>
+		<link rel="shortcut icon" href='favicon.ico'>
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "Результат вычисления разности квадратов двух чисел 5 и 8 равен ";
+				echo (5-8)*(5+8);
+			?>
+		</div>
+	</body>
+</html>
